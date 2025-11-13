338f4a696cab0813c57992a55313552c41573569
DanielePiccioni <danielepiccioni1@gmail.com>
2025-11-13 17:58:08 +0100

    add manage onChange for all input

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -8,27 +8,47 @@ export default function PostForm() {
         public: false,
     });
 
+    const handleChange = (e) => {
+        const { name, value, type, checked } = e.target;
+        setFormData({
+            ...formData,
+            [name]: type === "checkbox" ? checked : value,
+        })
+    }
+
     return (
         <form>
             <h2>Meow</h2>
             <div>
                 <label>Autore:</label>
-                <input type="text" name="author" />
+                <input
+                    type="text"
+                    name="author"
+                    value={formData.author}
+                    onChange={handleChange} />
             </div>
-
             <div>
                 <label>Titolo:</label>
-                <input type="text" name="title" />
+                <input
+                    type="text"
+                    name="title"
+                    value={formData.title}
+                    onChange={handleChange} />
             </div>
-
             <div>
                 <label>Testo:</label>
-                <textarea name="body"></textarea>
+                <textarea
+                    name="body"
+                    value={formData.body}
+                    onChange={handleChange}></textarea>
             </div>
-
             <div>
                 <label>
-                    <input type="checkbox" name="public" />
+                    <input
+                        type="checkbox"
+                        name="public"
+                        checked={formData.public}
+                        onChange={handleChange} />
                     Pubblico?
                 </label>
             </div>
